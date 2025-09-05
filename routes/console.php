2032aa94a8d597505556ab11975cab48715deb8f
Lars Åge Kamfjord <lars.age@kamfjord.org>
2025-09-05 16:12:41 +0200

    Changing how often the jobs are run

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-Schedule::job(new App\Jobs\GetWikidataTrackingJob())->daily();
-Schedule::job(new App\Jobs\GetCategoryCountsJob())->hourly();
+Schedule::job(new App\Jobs\GetWikidataTrackingJob())->hourlyAt(14);
+Schedule::job(new App\Jobs\GetCategoryCountsJob())->everyFiveMinutes;
 Schedule::command('horizon:snapshot')->everyFiveMinutes();
