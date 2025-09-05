daf4364f73ca9c6b645b699d868fe7fb930d621d
Lars Åge Kamfjord <lars.age@kamfjord.org>
2025-09-05 15:09:29 +0200

    Fix wrong setting for queue

--- config/queue.php
+++ config/queue.php
@@ -65,8 +65,8 @@ return [
 
         'redis' => [
             'driver' => 'redis',
-            'connection' => env('REDIS_QUEUE_CONNECTION', 'wikistats'),
-            'queue' => env('REDIS_QUEUE', 'default'),
+            'connection' => env('REDIS_QUEUE_CONNECTION', 'default'),
+            'queue' => env('REDIS_QUEUE', 'wikistats'),
             'retry_after' => (int) env('REDIS_QUEUE_RETRY_AFTER', 90),
             'block_for' => null,
             'after_commit' => false,
