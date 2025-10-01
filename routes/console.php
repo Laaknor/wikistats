aef04e7cad8a1ab357afa38352d66cab44d941e5
Lars Åge Kamfjord <lars.age@kamfjord.org>
2025-10-01 18:12:32 +0200

    Change how often schedule is run, based on need

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,10 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 Schedule::job(new App\Jobs\GetWikidataTrackingJob())->hourlyAt(14);
-Schedule::job(new App\Jobs\GetCategoryCountsJob())->everyFiveMinutes();
+Schedule::job(new App\Jobs\GetCategoryCountsJob())->everyMinute()
+    ->when(function() {
+        return App\Models\Category::where('last_sync','<',now()->subDays(7))->count() > 0;
+    });
 Schedule::command('horizon:snapshot')->everyFiveMinutes();
 Schedule::job(new App\Jobs\GetSiteInfoJob())->hourlyAt(24);
+#Schedule::job(new App\Jobs\GetUrlscanJob())->monthlyOn(1, '00:00');
